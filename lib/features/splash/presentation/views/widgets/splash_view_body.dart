465eeb2b1ee9f7eaa4a771f5f55ec5a294e9c96a
Hossam Ahmed <ha954074@gmail.com>
2025-03-22 00:01:44 +0200

    create home view and make navigator with get package and make a transition to next bage

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,11 @@
 import 'package:book_store/core/utils/assets.dart';
+import 'package:book_store/features/home/presentation/views/home_view.dart';
 import 'package:book_store/features/splash/presentation/views/widgets/sliding_text_animated.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../../../../constants.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,7 +21,14 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(vsync: this , duration: Duration(seconds: 1), );
+    initSlidingAnimation();
+    Future.delayed(const Duration(seconds: 4),() {
+      Get.to( () => const HomeView() , transition: Transition.rightToLeftWithFade,duration: kTransitionDuration);
+    },);
+  }
+
+  void initSlidingAnimation() {
+     animationController = AnimationController(vsync: this , duration: Duration(seconds: 1), );
     animation = Tween<Offset>(begin: Offset(0, 10), end: Offset.zero).animate(animationController);
     animationController.forward();
   }
